cc88ede6eecc36ad4e722a5a82ef6e3b81dd2bbe
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-05-25 20:33:10 +0530

    Certificates update

--- src/data/achievements.js
+++ src/data/achievements.js
@@ -9,42 +9,42 @@ export const certificates = [
     },
     {
         id: 2,
-        name: "Google Professional Cloud Developer",
-        issuer: "Google Cloud",
-        type: "Certification",
-        image: "/images/certificates/google-cloud-developer.png",
-        viewUrl: "https://www.credential.net/your-link"
+        name: "Introduction to Internet of Things (IoT)",
+        issuer: "NPTEL - IIT Kharagpur",
+        type: "Certification + Proctored Examination",
+        image: "/images/certificates/iot.png",
+        viewUrl: "https://drive.google.com/file/d/13FM0xHdPbbwyrhnWNl_jYOaehf0m2xyr/view"
     },
     {
         id: 3,
-        name: "Meta Front-End Developer Professional Certificate",
-        issuer: "Meta / Coursera",
-        type: "Certification",
-        image: "/images/certificates/meta-frontend.png",
-        viewUrl: "https://www.coursera.org/your-certificate"
+        name: "Cloud Computing",
+        issuer: "NPTEL - IIT Kharagpur",
+        type: "Certification + Proctored Examination",
+        image: "/images/certificates/cloud.png",
+        viewUrl: "https://drive.google.com/file/d/1Ijsml9cWVEN1NYYxd7spi03aJcJirFVM/view"
     },
     {
         id: 4,
-        name: "Deep Learning Specialization",
-        issuer: "DeepLearning.AI / Coursera",
-        type: "Certification",
-        image: "/images/certificates/deeplearning-specialization.png",
-        viewUrl: "https://www.coursera.org/your-certificate"
+        name: "Network Coding for Fault-Tolerant Transmission of Biomedical Data",
+        issuer: "International Conference on Intellingent Control, Computing, and Communication(IC3) | IEEE (February 13 - 14, 2025)",
+        type: "Research Paper - IEEE",
+        image: "/images/certificates/rp.png",
+        viewUrl: "https://ieeexplore.ieee.org/document/10956196"
     },
     {
         id: 5,
-        name: "TensorFlow Developer Certificate",
-        issuer: "Google / TensorFlow",
-        type: "Certification",
-        image: "/images/certificates/tensorflow-developer.png",
-        viewUrl: "https://www.credential.net/your-link"
+        name: "Dawn Of Code Hackathon (Finalists)",
+        issuer: "MernStack Club | Bhopal Municipal Corporation | Sheryians Coding School",
+        type: "Hackathon",
+        image: "/images/certificates/finalist.png",
+        viewUrl: "https://drive.google.com/file/d/1Hf4_qjNHhx4OrMPqlhsqjYb4d91a867U/view?usp=sharing"
     },
     {
         id: 6,
-        name: "Full Stack Open – University of Helsinki",
-        issuer: "University of Helsinki",
-        type: "Certification",
-        image: "/images/certificates/fullstack-open.png",
-        viewUrl: "https://studies.cs.helsinki.fi/stats/your-link"
+        name: "CM Award (Exceptional Academic Performance - Class XII)",
+        issuer: "CMO - ASSAM",
+        type: "Certificate of Appreciation",
+        image: "/images/certificates/cm-award.png",
+        viewUrl: "https://drive.google.com/file/d/1XoqijW22ikQ33NKgSyNZ7EvfZn9oshfW/view?usp=sharing"
     },
 ];
